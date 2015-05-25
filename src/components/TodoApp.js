8c738c02cf74aadbc9e60a24253ae508a9126f84
Manu <bionikspoon@gmail.com>
2015-05-24 23:45:06 -0500

    refactored list

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,12 +1,12 @@
 'use strict';
 
 import React from 'react/addons';
-import {Link} from 'react-router';
 
 import Actions from 'actions/TodoActionCreators';
 import TodoStore from 'stores/TodoStore';
-import TodoItem from 'components/TodoItem';
 import TodoForm from 'components/TodoForm';
+import TodoNav from 'components/TodoNav';
+import TodoList from 'components/TodoList';
 import debug from 'constants/DebugConstants';
 
 import 'styles/TodoApp.scss';
@@ -34,47 +34,15 @@ class TodoApp extends React.Component {
         Actions.updateText(event.target.value);
     }
 
-    removeAll(event) {
-        event.preventDefault();
-        Actions.removeAll();
-    }
-
-    removeArchived(event) {
-        event.preventDefault();
-        Actions.removeArchived();
-    }
 
 
-    filteredTodos = () => {
-        let returnAll = this.props.filter === 'all';
-        let archiveFilter = this.props.filter === 'archived';
 
-        return this.state.todos.filter((todo) => {
-            if (returnAll) return true;
-
-            return todo.archived === archiveFilter;
-        })
-    };
 
     render() {
         debug('Filter:', this.props.filter);
 
         debug("TodoApp State: ", this.state.todos, this.state.inputText);
-        let todos = this.filteredTodos().map((todo, index)=> {
-            let toggleArchive = Actions.toggleArchive.bind(this, todo, index);
-            let removeTodo = Actions.removeTodo.bind(this, index);
 
-            return (
-                <TodoItem key={todo._id}
-                          value={todo.text}
-                          archived={todo.archived}
-                          toggleArchive={toggleArchive}
-                          removeTodo={removeTodo} />
-            );
-        });
-
-        let inputText = (<TodoItem value={this.state.inputText}
-                                   preview="true" />);
 
         return (
 
@@ -84,44 +52,21 @@ class TodoApp extends React.Component {
                         <h1>Todo App</h1>
                     </div>
                     <div className="well well-lg">
-                        <ul className="list-group">
-
-                            {todos}
-                            {inputText}
-                        </ul>
+                        <TodoList todos={this.state.todos}
+                                  filter={this.props.filter}
+                                  toggleArchive={Actions.toggleArchive}
+                                  removeTodo={Actions.removeTodo}
+                                  inputText={this.state.inputText} />
 
                         <TodoForm toggleAll={Actions.toggleAll}
                                   value={this.state.inputText}
                                   onChange={this.updateText}
                                   onAddTodo={Actions.addTodo}
                                   allAreArchived={this.state.allAreArchived} />
-                        <ul className="nav nav-pills">
-                            <li role="presentation"
-                                className={this.props.filter === 'all' ? 'active':''}>
-                                <Link to="all">All</Link>
-                            </li>
-                            <li role="presentation"
-                                className={this.props.filter === 'archived' ? 'active':''}>
-                                <Link to="archived">Archived</Link>
-                            </li>
-                            <li role="presentation"
-                                className={this.props.filter === 'active' ? 'active':''}>
-                                <Link to="active">Active</Link>
-                            </li>
-                            <li role="presentation"
-                                className='pull-right'>
-                                <a className='remove-all'
-                                   href='#'
-                                   onClick={this.removeAll}>Remove All</a>
-                            </li>
-                            <li role="presentation"
-                                className='pull-right'>
-                                <a className='remove-archived'
-                                   href='#'
-                                   onClick={this.removeArchived}>Remove
-                                                                 Archived</a>
-                            </li>
-                        </ul>
+
+                        <TodoNav filter={this.props.filter}
+                                 removeAll={Actions.removeAll}
+                                 removeArchived={Actions.removeArchived} />
 
                     </div>
                 </div>
