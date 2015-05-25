f0f0ac33eb08a8128bd2acef0d5415f77fcb127d
Manu <bionikspoon@gmail.com>
2015-05-24 23:45:18 -0500

    refactored list

---
+++ test/spec/components/TodoList.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('TodoList', function () {
+  var React = require('react/addons');
+  var TodoList, component;
+
+  beforeEach(function () {
+    TodoList = require('components/TodoList.js');
+    component = React.createElement(TodoList);
+  });
+
+  it('should create a new instance of TodoList', function () {
+    expect(component).toBeDefined();
+  });
+});
---
+++ test/spec/components/TodoNav.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('TodoNav', function () {
+  var React = require('react/addons');
+  var TodoNav, component;
+
+  beforeEach(function () {
+    TodoNav = require('components/TodoNav.js');
+    component = React.createElement(TodoNav);
+  });
+
+  it('should create a new instance of TodoNav', function () {
+    expect(component).toBeDefined();
+  });
+});
